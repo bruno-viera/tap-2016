e8569907492c4f383248b08c2c04021bf877f644
Bruno Viera <viera707@gmail.com>
2016-04-21 14:58:17 -0300

    Algoritmos de Ordenação

---
+++ Ordenacao/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bt2_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            int numPosicoes = Convert.ToInt32(txtbox1.Text);
+
+            if(numPosicoes > 99)
+            {
+                for (int i = 0; i < numPosicoes; i++)
+                {
+                    listBox1.Items.Add(random.Next(numPosicoes).ToString());
+                }
+            }
+            else
+            {
+                MessageBox.Show("O vetor precisa ter no mínimo 100 posições!");
+            }
+        }
+
+        private void bt3_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+
+        private void bt1_Click(object sender, EventArgs e)
+        {
+            List<int> list = new List<int>();
+            Stopwatch tempo = new Stopwatch();
+
+            foreach (var item in listBox1.Items)
+            {
+                list.Add(Convert.ToInt32(item));
+            }
+
+            if (radioButton1.Checked)
+            {
+                tempo.Start();
+                clsOrdenacao.Shell(list);
+                tempo.Stop();
+            }
+            else if (radioButton2.Checked)
+            {
+                tempo.Start();
+                clsOrdenacao.Cocktail(list);
+                tempo.Stop();
+            }
+            else if (radioButton3.Checked)
+            {
+                tempo.Start();
+                clsOrdenacao.Insercao(list);
+                tempo.Stop();
+            }
+            else if (radioButton4.Checked)
+            {
+                tempo.Start();
+                clsOrdenacao.Comb(list);
+                tempo.Stop();
+            }
+            else
+            {
+                tempo.Start();
+                clsOrdenacao.Gnome(list);
+                tempo.Stop();
+            }
+
+            listBox1.Items.Clear();
+            foreach (var item in list)
+            {
+                listBox1.Items.Add(item.ToString());
+            }
+
+            label2.Text = tempo.Elapsed.ToString();
+        }
+    }
+}
---
+++ Ordenacao/WindowsFormsApplication1/WindowsFormsApplication1/clsOrdenacao.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    //Criada a classe clsOrdenacao
+    public static class clsOrdenacao
+    {
+        //Método que divide a lista a ser ordenada em grupos menores e aplica o método de ordenação por inserção
+        public static List<int> Shell (List<int> list)
+        {
+            //São declaradas as variáveis
+            int aux = 1;
+            int num = list.Count();
+            //laço de repetição
+            //condição dentro do parêntese 
+            while (aux < num)
+            {
+                aux = aux * 3 + 1;
+            }
+
+            aux = aux / 3;
+            int c, j;
+            while (aux > 0)
+            {
+                for (int i = aux; i < num; i++)
+                {
+                    c = list[i];
+                    j = i;
+                    while (j >= aux && list[j - aux] > c)
+                    {
+                        list[j] = list[j - aux];
+                        j = j - aux;
+                    }
+                    list[j] = c;
+                }
+                aux = aux / 2;
+            }
+            return list;
+        }
+
+        //Método que ordena a lista em ambas as direções
+        public static List<int> Cocktail(List<int> list)
+        {
+            //São declaradas as variáveis
+            int tam, comeco, final, troca, aux;
+            tam = list.Count();
+            comeco = 0;
+            final = tam - 1;
+            troca = 0;
+            //laço de repetição
+            //condição dentro do parêntese
+            while (troca == 0 && comeco < final)
+            {
+                troca = 1;
+                for (int i = comeco; i < final; i = i + 1)
+                {
+                    if (list[i] > list[i + 1])
+                    {
+                        aux = list[i];
+                        list[i] = list[i + 1];
+                        list[i + 1] = aux;
+                        troca = 0;
+                    }
+                }
+
+                final = final - 1;
+
+                for (int i = final; i > comeco; i = i - 1)
+                {
+                    if (list[i] < list[i - 1])
+                    {
+                        aux = list[i];
+                        list[i] = list[i - 1];
+                        list[i - 1] = aux;
+                        troca = 0;
+                    }
+                }
+
+                comeco = comeco + 1;
+            }
+
+            return list;
+        }
+
+        //Método de ordenação que percorre um vetor ordenando os elementos a esquerda a medida que avança
+        public static List<int> Insercao(List<int> list)
+        {
+            //variáveis
+            int x, aux;
+            //laço de repetição
+            //condição dentro do parêntese
+            for (int i = 1; i < list.Count(); i++)
+            {
+                x = i;
+                while ((x > 0) && (list[x - 1] > list[x]))
+                {
+                    aux = list[x - 1];
+                    list[x - 1] = list[x];
+                    list[x] = aux;
+                    x--;
+                }
+            }
+            return list;
+        }
+
+        // Método de ordenação por troca
+        public static List<int> Comb(List<int> list)
+        {
+            //variáveis
+            int aux = list.Count();
+            bool swapped = true;
+            //laço de repetição
+            //condição dentro do parêntese
+            while (aux > 1 || swapped)
+            {
+                if (aux > 1)
+                {
+                    aux = (int)(aux / 1.247330950103979);
+                }
+
+                int i = 0;
+                swapped = false;
+                while (i + aux < list.Count())
+                {
+                    if (list[i].CompareTo(list[i + aux]) > 0)
+                    {
+                        int t = list[i];
+                        list[i] = list[i + aux];
+                        list[i + aux] = t;
+                        swapped = true;
+                    }
+                    i++;
+                }
+            }
+
+            return list;
+        }
+
+        //Método similar ao de inserção, a diferença é que esse leva o elemento para a posição certa
+        public static List<int> Gnome(List<int> list)
+        {
+            //variáveis
+            int g = 0;
+            int aux;
+            //laço de repetição
+            //condição dentro do parêntese
+            while (g < (list.Count() - 1))
+            {
+                if (list[g] > list[g + 1])
+                {
+                    aux = list[g];
+                    list[g] = list[g + 1];
+                    list[g + 1] = aux;
+                    if (g > 0)
+                    {
+                        g -= 2;
+                    }
+                }
+                g++;
+            }
+
+            return list;
+        }
+    }
+}
